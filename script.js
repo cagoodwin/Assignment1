c0b9623cec82ba787ac0281514e2389954fb1ba8
cagoodwin <careyanne@gmail.com>
2015-09-16 19:29:32 -0700

    assignment 1 parts 1 and 2

---
+++ script.js
@@ -0,0 +1,145 @@
+/*//STEP 1
+someMonth
+currentMonth
+currentMONTH
+summerMonths
+myLibrary*/
+
+/*//STEP 2
+107
+"Howdy"
+flase
+null*/
+
+/*//STEP 3
+var complexExpres = 1 + (2*23);
+var complexExpress2 = complexExpres % 2;*/
+
+/*//STEP 4
+var strFirstName, strLastName, strAddress, strCity, strState, numZipCode, numYourAge, strReferralSource, strMayWeContactYou;*/
+
+/*
+//STEP 5
+var strFirstName;
+var strLastName;
+var strAddress;
+strFirstName = "Carey";
+strLastName = "Goodin";
+strAddress = "123 Main Street";
+
+var strCity = "San Diego";
+var strState = "CA";
+var numZipCode = "92117";
+
+var numYourAge="30", strReferralSource="my mom", strMayWeContactYou="yes";
+*/
+
+/*//STEP 6
+var numString = 4 + "dogs";
+console.log(numString);
+
+var booString = true + "dat";
+console.log(booString);
+
+var numBoolean = 99 + false;
+console.log(numBoolean);*/
+
+/*//STEP 7
+console.log(dog);
+var dog;
+dog = "I love dogs";*/
+
+//STEP 8
+//It is not valid. You would need to escape characters to keep the apostrophe and the quotation marks otherwise it will rad it as seperate strings. It should be:
+
+/*
+var someString = 'Who once said, \"Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former.\"';
+console.log(someString);
+*/
+
+/*//STEP 9
+var x = null;
+console.log(x);
+var y;
+console.log(x+y);*/
+
+/*//STEP 10
+console.log(typeof "Hello World");
+console.log(typeof 3467);
+console.log(typeof true);
+console.log(typeof new Date());
+console.log(typeof myDog);*/
+
+/*
+//STEP 11
+alert("Hello" + " Carey," + " welcome to the JavaScript class!");
+*/
+
+/*//STEP 12
+var name = "Carey Goodwin";
+alert("Hello " + name + ", welcome to the JavaScript class!");*/
+
+/*//STEP 13
+var name = "Carey Goodwin";
+var course = "JavaScript";
+alert("Hello " + name + ", welcome to the " + course + " class!");*/
+
+/*//STEP 14
+var name = "Carey Goodwin";
+var course = "JavaScript";
+alert("Hello " + name + ".\nWelcome to the " + course + " class!");*/
+
+/*//STEP 15
+var name = prompt("What is your name?");
+alert("Hello " + name + ", welcome to the JavaScript class!")*/
+
+/*//STEP 16
+var name = prompt("What is your name?");
+var course = prompt("What class are you taking?");
+alert("Hello " + name + ".\nWelcome to the " + course + " class!");*/
+
+/*//STEP 17
+var x = 10;
+var y = 20;
+console.log(x+y);*/
+
+/*//STEP 18
+var x = 20;
+x+=20;
+console.log(x);*/
+
+/*//STEP 19
+var x = 20;
+x*=5;
+console.log(x);*/
+
+/*//STEP 20
+var x = 20 % 3;
+x/=1;
+console.log(x);*/
+
+/*//STEP 21
+var x = 5;
+var y = 10;
+console.log(x<=y);*/
+
+/*
+//STEP 22
+var x = 5;
+var y = 10;
+console.log (x==y && x>y);
+*/
+
+/*//STEP 23
+var widget = new Object();
+console.log(typeof widget);*/
+
+/*
+//STEP 24
+var widget = new Object();
+console.log(widget instanceof Object);
+*/
+
+/*//STEP 25
+var widget = new Object();
+console.log(widget instanceof String);*/
